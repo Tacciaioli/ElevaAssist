8e5d6925af13f519b35c5bc2ae85e799e6d6f34f
Tiago Acciaioli <tiagoacciaioli1@gmail.com>
2023-06-27 23:05:04 +0100

    links and trys

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,5 @@
+# require_relative 'config/openai'
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home , :page ]
 
@@ -5,6 +7,30 @@ class PagesController < ApplicationController
   end
 
   def page
+    client = OpenAI::Client.new
+    # response = OpenAI.Completion.create(
+    # engine: 'text-davinci-003',
+    # prompt: 'como posso aprender ruby?',
+    # max_tokens: 100
+    # )
+
+    # # @generated_text = response.choices.first.text.strip
+    # response = client.completions(
+    #   parameters: {
+    #     model: "text-davinci-001",
+    #     prompt: "Once upon a time",
+    #     max_tokens: 5
+    #   })
+    #   puts response["choices"].map { |c| c["text"] }
+    response = client.chat(
+  parameters: {
+    model: "gpt-3.5-turbo",
+    messages: [
+      { role: "user", content: "Qual é a capital da França?" }
+    ]
+  }
+)
+puts response.dig("choices", 0, "message", "content")
   end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 require_relative "boot"
+require "openai"
 
 require "rails/all"
 
@@ -12,6 +13,7 @@ module ElevaAssist
       generate.assets false
       generate.helper false
       generate.test_framework :test_unit, fixture: false
+
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
@@ -23,5 +25,11 @@ module ElevaAssist
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    OpenAI.configure do |config|
+      config.access_token = ENV.fetch("OPENAI_ACCESS_TOKEN")
+      # config.organization_id = ENV.fetch("OPENAI_ORGANIZATION_ID", nil) # Opcional.
+    end
+    # client = OpenAI::Client.new(access_token: "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")
+
   end
 end
